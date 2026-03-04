11ae767645eb301614feb9c3a23ed6140f206488
Awais Maqbool <awaismaqbool579@gmail.com>
2026-03-04 10:48:10 +0500

    updated project

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from langchain-chatbot!")
+from langchain_ollama import ChatOllama
 
+llm = ChatOllama(
+    model="gemma:2b",
+    temperature=0,
+    # other params...
+)
 
-if __name__ == "__main__":
-    main()
+response = llm.invoke('waht is rag')
+print(response.content)
